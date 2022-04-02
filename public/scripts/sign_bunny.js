e1a3d257e2627a6c4fa22b9cf07a2d815a85808c
Zachary Churchill <zacharyachurchill@gmail.com>
2022-04-01 20:30:34 -0400

    sign width val text

--- public/scripts/sign_bunny.js
+++ public/scripts/sign_bunny.js
@@ -2,7 +2,9 @@ ready(() => {
   let signBunnyDiv = document.getElementById('signBunny')
   let signText = document.getElementById('signText')
   let signWidth = document.getElementById('signWidth')
+  let signWidthVal = document.getElementById('signWidthVal')
   function changeSign() {
+    signWidthVal.innerHTML = signWidth.value.toString()
     signBunnyDiv.innerHTML = signBunny(signText.value,signWidth.value).replace(/(\r\n|\n|\r)/gm, "<br>")
   }
   changeSign()
